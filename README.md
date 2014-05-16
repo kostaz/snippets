493920fcda1cf820aa42c3d678cb6005773d018e
Kosta Zertsekel <konszert@marvell.com>
2014-05-16 12:15:25 +0300

    Add SHA256 example in C

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 sha1.c
 gcc sha1.c -o sha1 -lcrypto
 
+sha256.c
+gcc sha256.c -o sha256 -lcrypto
+
---
+++ sha256.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+int main()
+{
+	unsigned char digest[SHA256_DIGEST_LENGTH];
+	unsigned char ibuf[] = "hello world";
+	unsigned char obuf[SHA256_DIGEST_LENGTH * 2 + 1];
+	int i;
+
+	if (!SHA256(ibuf, strlen(ibuf), digest)) {
+		printf("%s: SHA256 failed\n", __func__);
+		return 1;
+	}
+
+	for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+		sprintf(&obuf[i * 2], "%02x", digest[i]);
+	printf("SHA256 digest: 0x%s\n", obuf);
+
+	return 0;
+}
