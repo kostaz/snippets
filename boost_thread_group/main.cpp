75dbdff9891446b6ad1df9ac86c9f13a169e9fcf
Kosta Zertsekel <zertsekel@gmail.com>
2014-05-17 11:05:28 +0300

    Add boost::thread_group example (Part 2)

--- boost_thread_group/main.cpp
+++ boost_thread_group/main.cpp
@@ -27,6 +27,34 @@ int main()
 			threads.create_thread(&increment_count);
 		threads.join_all();
 	}
+
+#ifdef BOOST_THREAD_PROVIDES_INTERRUPTIONS
+#warning BOOST_THREAD_PROVIDES_INTERRUPTIONS are enabled!
+	{
+		boost::thread_group threads;
+		for (int i = 0; i < 3; i++)
+			threads.create_thread(&increment_count);
+		threads.interrupt_all();
+		threads.join_all();
+	}
+#endif
+
+	{
+		boost::thread_group threads;
+		boost::thread *th = new boost::thread(&increment_count);
+		threads.add_thread(th);
+		threads.join_all();
+	}
+
+	{
+		{
+			boost::unique_lock<boost::mutex> lock(mutex);
+			boost::thread *th2 = new boost::thread(&increment_count_2);
+			threads2.add_thread(th2);
+		}
+		threads2.join_all();
+	}
+
 	return 0;
 }
 
